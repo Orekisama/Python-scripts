5533732aa6430a4e0ba4e41b8c798d9d248c3795
Orekisama <michael2cindy@163.com>
2018-06-04 16:38:18 +0800

    当关键词为一整行的时候

--- sed_1.py
+++ sed_1.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# 当keyword是一整行的时候
 
 f = open('object.log', 'r', encoding='utf-8')
 r = open('output.log', 'w', encoding='utf-8')
 lines = f.read() 
 keyword='---Tl8PvPyi---A--'
 log = lines.split(keyword)
-print(log[-1])
+r.write(keyword + log[-1])
